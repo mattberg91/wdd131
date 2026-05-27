349c04bb1f016f54676e87f5c0ff20edd5b52a19
mattberg91 <mattberg1991@gmail.com>
2026-05-26 22:57:44 -0600

    fixed a part I missed on the video.

--- ponder/arrays.js
+++ ponder/arrays.js
@@ -26,7 +26,7 @@ function listTemplate(item) {
 myList.innerHTML = stepsHtml.join('');
 
 
-let grades = ['A', 'B', 'C'];
+let grades = ['A', 'A', 'A'];
 let points;
 
 let gpaPoints = grades.map(convert);
