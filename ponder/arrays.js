1030f7c94b4b3627441ae943ba6501240f9b978a
mattberg91 <mattberg1991@gmail.com>
2026-05-26 21:58:09 -0600

    creating the arrays js page.

---
+++ ponder/arrays.js
@@ -0,0 +1,35 @@
+//.forEach
+
+const steps = ['one', 'two', 'three'];
+
+
+//steps.forEach(function(item){
+//    console.log(item);
+//});
+
+steps.forEach(showSteps);
+
+function showSteps(step){
+    console.log(step);
+};
+
+//.map
+
+let myList = document.querySelectorAll('#myList');
+
+const stepsHtml = steps.map(listTemplate);
+
+function listTemplate(item) {
+    return `<li>${item}</li>`;
+};
+
+myList.innerHTML = stepsHtml.join('');
+
+
+
+
+
+
+
+
+
