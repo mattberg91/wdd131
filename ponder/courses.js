937df2801f37b48657afbabbe985ae155a974721
mattberg91 <mattberg1991@gmail.com>
2026-06-11 00:10:29 -0600

    making changes to the js to do more modular functions.

--- ponder/courses.js
+++ ponder/courses.js
@@ -7,7 +7,12 @@ const aCourse = {
     sections: [
     { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
     { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}
-    ]
+    ],
+    
+    enrollStudent: function () {
+            console.log('student enrolled');
+        }
+            
             
   };
             
@@ -21,6 +26,18 @@ document.querySelector('courseCodee').textContent = aCourse.code;
 document.querySelector('img').setAttribute('src', aCourse.logo);
 document.querySelector('img').setAttribute('alt', aCourse.name);
 document.querySelector('img').style.width= '100px';
+
+console.log(aCourse.sections[1].roomNum);
+
+aCourse.enrollStudent();
+
+
+
+
+
+
+
+
 
 
 
