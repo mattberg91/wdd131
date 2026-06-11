de0618d6822a2675c7f5c673664fe40c9455598e
mattberg91 <mattberg1991@gmail.com>
2026-06-10 21:09:50 -0600

    creating the nested objects.

--- ponder/courses.js
+++ ponder/courses.js
@@ -2,7 +2,13 @@
 const aCourse = {
     code: 'CSE121b',
     name: 'Javascript Language',
-    logo: 'images/js-logo.png'
+    logo: 'images/js-logo.png',
+    
+    sections: [
+    { sectionNum: 1, roomNum: 'STC 353', enrolled: 26, days: 'TTh', instructor: 'Bro T'},
+    { sectionNum: 2, roomNum: 'STC 347', enrolled: 28, days: 'TTh', instructor: 'Sis A'}
+    ]
+            
   };
             
 console.log(aCourse.code);
@@ -12,7 +18,9 @@ console.log(aCourse.name);
 document.querySelector('courseName').textContent = aCourse.name;
 document.querySelector('courseCodee').textContent = aCourse.code;
 
-
+document.querySelector('img').setAttribute('src', aCourse.logo);
+document.querySelector('img').setAttribute('alt', aCourse.name);
+document.querySelector('img').style.width= '100px';
 
 
 
