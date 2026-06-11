d8a30fcf82d06f9824aa278403e43a35374c60d3
mattberg91 <mattberg1991@gmail.com>
2026-06-10 21:06:08 -0600

    creating various object js items

---
+++ ponder/courses.js
@@ -0,0 +1,19 @@
+
+const aCourse = {
+    code: 'CSE121b',
+    name: 'Javascript Language',
+    logo: 'images/js-logo.png'
+  };
+            
+console.log(aCourse.code);
+console.log(aCourse.name);
+
+
+document.querySelector('courseName').textContent = aCourse.name;
+document.querySelector('courseCodee').textContent = aCourse.code;
+
+
+
+
+
+
