f0ba43a585b4b61041ef823028018c2b8e0c3137
mattberg91 <mattberg1991@gmail.com>
2026-06-11 21:50:49 -0600

    trying to make the javascript for the character sheet.

---
+++ charactercard/charactercard.js
@@ -0,0 +1,59 @@
+
+const characterCard = {
+    name: 'character-card',
+    logo: 'snortleblat.png',
+    
+    
+    
+    
+    
+    
+    stats: [
+        { statName: 'Level', statValue: 5 },
+        { statName: 'Health', statValue: 100 },
+    ],
+
+    
+
+};
+
+
+
+
+
+
+
+
+document.querySelector('img').setAttribute('src', characterCard.logo);
+document.querySelector('img').setAttribute('alt', characterCard.name);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
