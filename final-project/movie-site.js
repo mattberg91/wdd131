98de6d5cf0e08a85d65c3e99424de8959b8cd22f
mattberg91 <mattberg1991@gmail.com>
2026-07-12 22:14:08 -0600

    Missed my js error of adding the dates to the page and changing the color of the search icon.

--- final-project/movie-site.js
+++ final-project/movie-site.js
@@ -12,16 +12,19 @@ let movieList = [
         name: "Frozen",
         description: "A movie about a cursed princess who controls ice and goes on an adventure with her sister.",
         rating: "PG",
+        date: "7/12/2026"
     },
     {
         name: "Tangled",
         description: "A movie about a princess with magic hair that heals people who goes on an adventure to find out who she is.",
         rating: "PG",
+        date: "7/12/2026"
     },
     {
         name: "My Hero Academia: Two Heroes",
         description: "A movie about a young heroS and his mentor visiting an island made out of technology and their ensuing adventure.",
         rating: "PG-13",
+        date: "7/12/26"
     },
 ]
 
@@ -46,69 +49,67 @@ form.addEventListener("submit", function (event) {
     movieList.push(newMovie);
     movieList.sort(compareMovies);
     saveMovies();
-    
+
     console.log(movieList);
     output.innerHTML = `
         <h2>Movie Submitted</h2>
         <p>Movie Name: ${movieName}</p>
         <p>Movie Description: ${movieDescription}</p>
         <p>Movie Rating: ${movieRating}</p>
-        ${
-            movieDate
-                ? `<p><strong>Date Added:</strong> ${movieDate}</p>`
-                : ""
+        ${movieDate
+            ? `<p><strong>Date Added:</strong> ${movieDate}</p>`
+            : ""
         }
         `;
 
 
 
-        init();
+    init();
 
-        form.reset();
+    form.reset();
 });
 
 searchForm.addEventListener("submit", search)
 
 function search(event) {
-        event.preventDefault();
+    event.preventDefault();
 
-        let searchterm = document.querySelector('#search').value;
+    let searchterm = document.querySelector('#search').value;
 
-        let filteredMovies = movieList.filter(function(movie){
-            return (
-                movie.name.toLowerCase().includes(searchterm.toLowerCase()) ||
-                movie.description.toLowerCase().includes(searchterm.toLowerCase()) ||
-                movie.rating.toLowerCase().includes(searchterm.toLowerCase())
-            );
-        });
-        console.log(filteredMovies);
+    let filteredMovies = movieList.filter(function (movie) {
+        return (
+            movie.name.toLowerCase().includes(searchterm.toLowerCase()) ||
+            movie.description.toLowerCase().includes(searchterm.toLowerCase()) ||
+            movie.rating.toLowerCase().includes(searchterm.toLowerCase())
+        );
+    });
+    console.log(filteredMovies);
 
-        let sortedMovies = filteredMovies.sort(compareMovies)
+    let sortedMovies = filteredMovies.sort(compareMovies)
 
 
-        renderMovies(sortedMovies);
-    }
-
-    function compareMovies(a, b) {
-        if (a.name < b.name) {
-            return -1;
-        }else if (a.name > b.name) {
-            return 1;
-        }
-        return 0;
+    renderMovies(sortedMovies);
 }
 
-    function movieTemplate(movie) {
-        return `
+function compareMovies(a, b) {
+    if (a.name < b.name) {
+        return -1;
+    } else if (a.name > b.name) {
+        return 1;
+    }
+    return 0;
+}
+
+function movieTemplate(movie) {
+    return `
             <article class="movieCard">
                 <h3><strong>Name: </strong>${movie.name}</h3>
                 <p><strong>Description: </strong>${movie.description}</p>
                 <p><strong>Rating:</strong> ${movie.rating}</p>
-                ${
-                    movie.date
-                    ? `<p><strong>Date added:</strong> ${movie.date}</p>`
-                    : ""
-                }
+                ${movie.date
+            ? `<p><strong>Date added:</strong> ${movie.date}</p>`
+            : ""
+        }
             </article>
             `;
 }
@@ -129,7 +130,7 @@ function loadMovies() {
 
 function renderMovies(movies) {
     movieCard.innerHTML = "";
-    movies.forEach(function(movie) {
+    movies.forEach(function (movie) {
         movieCard.innerHTML += movieTemplate(movie);
     });
 }
@@ -143,8 +144,8 @@ function getRandomMovie() {
 
 function init() {
     loadMovies();
-        let randomMovie = getRandomMovie();
-        renderMovies([randomMovie]);
+    let randomMovie = getRandomMovie();
+    renderMovies([randomMovie]);
 }
 
 
